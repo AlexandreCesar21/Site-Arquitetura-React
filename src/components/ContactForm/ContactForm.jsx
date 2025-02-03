3a177eeb02d22c4184fe8a6ee94410f09f976196
xandebarros9@gmail.com <127257670+AlexandreCesar21@users.noreply.github.com>
2025-02-02 22:25:14 -0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,6 +7,45 @@ import Button from "../Button/Button"
 
 
 function ContactForm(){
+    const [FormData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })    
+    const [isFormValid, setIsFormValid] = useState(false)
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if(isFormValid){
+            null
+        }
+    }
+
+    useEffect(() => {
+        const isValidEmail = (email) => {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email) 
+        }
+
+
+        const isValid = FormData.name.trim() && 
+        FormData.email.trim() && 
+        isValidEmail(FormData.email) && 
+        FormData.message.trim()
+
+        setIsFormValid(isValid)
+
+    }, [FormData])
+
+    const handleChange = (e) => {
+        const {name, value } = e.target
+        setFormData({
+            ...FormData,
+            [name]: value
+        })
+    }
+
 return(
     
         <div className="contact-form d-flex fd-column al-center">
@@ -19,6 +58,7 @@ return(
                         id="name"
                         name="name"
                         placeholder="Name *"
+                        onChange={handleChange}
                     />
                     <input 
                         className="form-input" 
@@ -26,6 +66,7 @@ return(
                         id="email"
                         name="email"
                         placeholder="Email *" 
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="d-flex form-group">
@@ -34,11 +75,12 @@ return(
                         id="messagem"
                         name="message"
                         placeholder="Mensagem *"
+                        onChange={handleChange}
                         rows="4"  
                     ></textarea>
                 </div>
                 <div className="al-center d-flex jc-end form-group ">
-                <Button type="submit" buttonStyle="secondary"> 
+                <Button type="submit" buttonStyle="secondary" disabled={!isFormValid}> 
                             Enviar 
                         </Button>
                 </div>
