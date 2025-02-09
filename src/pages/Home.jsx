8d0583ea0aa2f5c75ec28243fdfb857f74002e88
Alexandre César <xandebarros9@gmail.com>
2025-02-09 18:41:18 -0300

    mod

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import ProjectsList from "../components/ProjectList/ProjectsLists";
-import Footer from "../components/footer/footer";
+import Footer from "../components/Footer/Footer";
 
 function Home(){
     return(
