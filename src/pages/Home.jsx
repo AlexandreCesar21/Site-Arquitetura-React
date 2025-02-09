9ec3470c6032fc682f6db5f49e414f3aa38476b0
Alexandre César <xandebarros9@gmail.com>
2025-02-09 18:15:22 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
-import Footer from "../components/Footer/Footer";
+import Footer from "../components/footer/footer";
 import ProjectsList from "../components/ProjectList/ProjectsLists";
 
 
