a7ec38b489de8088ee118c53d3d8190d907c77e4
Alexandre César <xandebarros9@gmail.com>
2025-02-09 19:36:41 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
-import Header from "../components/Header/Header";
-import Hero from "../components/Hero/Hero";
-import ProjectsList from "../components/ProjectList/ProjectsLists";
-import Footer from "../components/Footer/Footer.jsx";
+    import Header from "../components/Header/Header";
+    import Hero from "../components/Hero/Hero";
+    import ProjectsList from "../components/ProjectList/ProjectsLists";
+    import Footer from "../components/Footer/Footer.jsx";
 
-function Home(){
-    return(
-        <>
-            <Header/>
-            <div className="container">
-                <Hero/>
-                <ProjectsList/>
-            </div>
-            <Footer/>
-        </>
-    )
-}
+    function Home(){
+        return(
+            <>
+                <Header/>
+                <div className="container">
+                    <Hero/>
+                    <ProjectsList/>
+                </div>
+                <Footer/>
+            </>
+        )
+    }
 
-export default Home
+    export default Home
