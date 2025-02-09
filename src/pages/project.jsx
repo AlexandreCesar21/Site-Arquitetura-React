d1176beb052a21cfef989eba1600ecc5cde76114
Alexandre César <xandebarros9@gmail.com>
2025-02-09 19:45:41 -0300

    md

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,6 +1,6 @@
 import Banner from "../components/Banner/Banner";
 import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+import Footer from "../components/Footer/Footer.jsx";
 import ProjectsList from "../components/ProjectList/ProjectsLists";
 
 
